5c21859b3e4573695c74418942c0a475f26d8420
Jackson-Wozniak <jjw30@comcast.net>
2023-01-05 22:57:54 -0500

    initial files for commit

---
+++ backend/src/main/java/com/api/memory_quizzes/MemoryQuizzesApplication.java
@@ -0,0 +1,13 @@
+package com.api.memory_quizzes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryQuizzesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryQuizzesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/api/memory_quizzes/MemoryQuizzesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.memory_quizzes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryQuizzesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
