dc625106d5ed10113ff60df72730db10bb17c48b
Jackson-Wozniak <jjw30@comcast.net>
2023-01-08 11:02:48 -0500

    add test to verify total likes/plays query

---
+++ backend/src/test/java/com/api/memory_quizzes/service/GameStatsServiceTest.java
@@ -0,0 +1,38 @@
+package com.api.memory_quizzes.service;
+
+import com.api.memory_quizzes.entity.GameStats;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class GameStatsServiceTest {
+
+    @Autowired
+    private GameStatsService gameStatsService;
+
+    @Test
+    void totalGamesPlayedQueryWorks(){
+        List<GameStats> gameStats = gameStatsService.findAllStats();
+        long totalGamesPlayed = gameStats.stream()
+                .map(GameStats::getTimesPlayed)
+                .reduce(0L, Long::sum);
+        assertEquals(totalGamesPlayed, gameStatsService.getTotalGamesPlayed());
+    }
+
+    @Test
+    void totalLikedQueryWorks(){
+        List<GameStats> gameStats = gameStatsService.findAllStats();
+        long totalLiked = gameStats.stream()
+                .map(GameStats::getTimesLiked)
+                        .reduce(0L, Long::sum);
+        assertEquals(totalLiked, gameStatsService.getTotalLikes());
+    }
+}
